a9139895f7a6ff6b46f550acc902fcaf79d8c64f
Tipotoronti <133032670+Tipotoronti@users.noreply.github.com>
2023-05-23 10:53:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-
+const charactersWithoutNumbers = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 let placeholderOneEl = document.getElementById("placeholder-one-el")
 let placeholderTwoEl = document.getElementById("placeholder-two-el")
 let savePassword = 0
+let withoutSymbols = false;
+let withoutNumbers = false;
+
+const switchElementone = document.getElementById("checkone");
+const switchElementtwo = document.getElementById("checktwo");
+
+switchElementone.addEventListener("change", function() {
+    if (this.checked) {
+         withoutSymbols = true;    
+    } else {
+        withoutSymbols = false;
+        return withoutSymbols
+    }
+  });
+
+switchElementtwo.addEventListener("change", function() {
+    if (this.checked) {
+       withoutNumbers = true;    
+    } else {
+        withoutNumbers = false;
+        return withoutNumbers
+    }
+  });
+
+
 
 function getRandomCharacter() {
-    let randomindex = Math.floor(Math.random()*characters.length);
-    return characters[randomindex]
+    if (withoutSymbols === false && withoutNumbers === false) {
+        let randomindex = Math.floor(Math.random()*characters.length);
+        return characters[randomindex]
+    } else if (withoutSymbols === true && withoutNumbers === false) {
+        let randomindex = Math.floor(Math.random()*62);
+        return characters[randomindex]
+    } else if (withoutSymbols === true && withoutNumbers === true) {
+        let randomindex = Math.floor(Math.random()*52);
+        return characters[randomindex]
+    } else if (withoutSymbols === false && withoutNumbers === true) {
+        let randomindex = Math.floor(Math.random()*charactersWithoutNumbers.length);
+        return charactersWithoutNumbers[randomindex]
+    }
 }
-
 
 function generatePasswords() {
     placeholderOneEl.textContent = " ";
@@ -57,3 +92,6 @@ placeholderTwoEl.addEventListener('click', copyPassword);
         console.error('Failed to copy: ', err);
       });
   }
+
+
+
